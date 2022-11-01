51879aa33d86c0651e9a9fdd013c5423f0d4b8bb
Max Wank <maxwank11@gmail.com>
2022-11-01 12:11:46 -0400

    finished with inputs

--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -1,25 +1,45 @@
 import java.util.Scanner;
 public class LinearSearch {
-    public static Scanner sc = new Scanner(System.in);
-    public static int find;
+    public static Scanner sc = new Scanner(System.in); //scanner for letter
+   // Scanner scl = new Scanner (System.in); //scanner for character
+    public static int find; // find value
+    public static float valueAmt = 0;
+    public static int arrayLength;
+    public static int[] values = new int[arrayLength];
+
+
     public static void main(String[] args) {
+        collectValues();
         select();
         search();
+    }
+    public static void clear() {
 
+        System.out.print("\033[H\033[2J");
+    }
+    public static void wait(int ms) {
+        try
+        {
+            Thread.sleep(ms);
+        }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
+        }
     }
     public static void search(){
         {
-            // 0 1 2 3 4 5
-            int[] numbers={2,9,5,6,7,2};
+
+            //int[] numbers;
             int searchValue = find;
             boolean found=false;
 
             //loop counter from 0 to 5, up to and including 5
-            for(int counter=0; counter <= 5; counter++) {
-                if (numbers[counter]==searchValue)
+            for(int counter=0; counter <= (valueAmt-1); counter++) {
+                if (values[counter]==searchValue)
                 {
                     found=true;
-                    System.out.println(searchValue + " was found at " + counter);
+                    System.out.println(searchValue + " was found at position " + counter + ".");
                 }
             }
             if (!found){
@@ -30,6 +50,19 @@ public class LinearSearch {
     public static void select(){
         System.out.println("Type # to find");
          find = sc.nextInt();
-
     }
+    public static void collectValues(){
+        System.out.println("How many values required?");
+        arrayLength=sc.nextInt();
+        values=new int[arrayLength];
+        for (int i = 0; i < arrayLength; i++) {
+            System.out.println("Type in values. Press enter after each value. #'s entered: " + (valueAmt));
+            valueAmt++;
+            values[i] = sc.nextInt();
+            wait(100);
+            clear();
+
+        }
+    }
+
 }
